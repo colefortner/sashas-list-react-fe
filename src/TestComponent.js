7f8bd1c54070806b2db15ac4372ffc784b0d7688
Cole Fortner <colefortner@gmail.com>
2021-12-27 17:20:12 -0500

    Wire up test component with local backend and initialize firebase

--- src/TestComponent.js
+++ src/TestComponent.js
@@ -1,14 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const TestComponent = (props) => {
+  const [businessData, setBusinessData] = useState();
   useEffect(() => {
-    fetch("http://localhost:5555/api/businesses/1").then(async (response) => {
-      const data = await response.json();
-      console.log(data);
-    });
+    // fetch("http://localhost:5555/api/businesses/1")
+    fetch("https://sashas-list.heroku.com/api/businesses/1")
+      .then(async (response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        // const data = await response.json()
+        // console.log(data);
+        // setBusinessData(data.business);
+        // console.log(businessData);
+      })
+      .then((data) => {
+        setBusinessData(data.business);
+      });
   }, []);
 
-  return <div>Testing</div>;
+  return (
+    <div>
+      {businessData ? (
+        <div>
+          <p>{businessData.id}</p>
+          <p>{businessData.name}</p>
+          <p>{businessData.description}</p>
+        </div>
+      ) : (
+        <p>hello</p>
+      )}
+    </div>
+  );
 };
 
 export default TestComponent;
